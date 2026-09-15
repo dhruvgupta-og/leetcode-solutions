de1de9000f028ee5c7e4d6a8c9b436b510af540d
Dhruv Gupta <aspireskull@gmail.com>
2026-09-15 21:23:34 +0530

    Time: 4 ms (56.61%) | Memory: 21.9 MB (99.19%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPal(string& s, int i, int j) {
+        while (i < j) {
+            if (s[i++] != s[j--])
+                return false;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s[i] != s[j])
+                return isPal(s, i + 1, j) || isPal(s, i, j - 1);
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
